954d459127ce6b373d56178e21e688f0d305026d
Mano <mano@xkanda.com>
2017-12-13 22:15:51 +0530

    Button with Image.

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
-const MainApp = (props) => {
-  const name = '01. Singh is King';
+class MainApp extends Component {
+  name = '01. Singh is King';
+  heart = require('./img/plain-heart.png');
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.innerContainer} />
-      <Text style={styles.title}>
-        <Text style={styles.subtitle}>Playing:</Text> {name}
-      </Text>
-    </View>
-  );
+  state = {
+    liked: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.onButtonPressed = this.onButtonPressed.bind(this);
+  }
+
+
+  onButtonPressed = () => {
+    console.warn("Button Pressed.");
+    this.setState({
+      liked : !this.state.liked,
+    });
+  };
+
+  render() {
+    const likedStyles = (this.state.liked) ? styles.like : null;
+
+    return ( 
+      <View style={styles.container}>
+        <View style={styles.innerContainer} />
+        <Text style={styles.title}>
+          <Text style={styles.subtitle}>Playing:</Text> {this.name}
+        </Text>
+        <TouchableHighlight
+          style={styles.btn}
+          underlayColor="#fefefe"
+          onPress={this.onButtonPressed}>
+          <Image source={this.heart} style={[styles.icon, likedStyles]}/>
+        </TouchableHighlight>
+      </View>
+    );
+  };
+
 };
 
 const styles = StyleSheet.create({
@@ -20,11 +50,12 @@ const styles = StyleSheet.create({
     marginTop: 100,
     backgroundColor: '#e67e22',
     borderRadius: 5,
+    alignItems: 'center',
   },
   innerContainer: {
-    backgroundColor: '#00ff00',
+    backgroundColor: 'transparent',
     height: 50,
-    width: 350,
+    width: 150,
     borderTopLeftRadius: 5,
     borderBottomLeftRadius: 5,
   },
@@ -39,6 +70,18 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontWeight: 'bold',
+  },
+  btn: {
+    borderRadius: 5,
+    padding: 10,
+  },
+  icon: {
+    width: 180,
+    height: 180,
+    tintColor: '#f1f1f1',
+  },
+  like: {
+    tintColor: '#e74c3c',
   },
 });
 
